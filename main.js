a4b41834f9fa151407ef486769ba8951064da229
Eliah Kagan <degeneracypressure@gmail.com>
2021-09-03 10:11:47 -0400

    improved input validation

--- main.js
+++ main.js
@@ -16,32 +16,50 @@
     const resultMod = document.getElementById('result-mod');
     const result = document.getElementById('result');
 
-    const parse = function (input) {
+    const parse = function (input, rangeCheck = (value) => true) {
         const text = input.value.trim();
         if (text.length === 0) {
             throw new SyntaxError('refusing to parse empty string as zero');
         }
-        return BigInt(text);
+
+        const value = BigInt(text);
+        if (!rangeCheck(value)) {
+            throw new RangeError('an argument is out of range');
+        }
+
+        return value;
+    };
+
+    const clearResults = function () {
+        resultBase.innerText = NO_VALUE;
+        resultExponent.innerText = NO_VALUE;
+        resultMod.innerText = NO_VALUE;
+        result.innerText = NO_VALUE;
+    };
+
+    const setResults = function (base, exponent, mod, power) {
+        resultBase.innerText = String(base);
+        resultExponent.innerText = String(exponent);
+        resultMod.innerText = String(mod);
+        result.innerText = String(power);
     };
 
     const update = function () {
         try {
             const base = parse(inputBase);
-            const exponent = parse(inputExponent);
-            const mod = parse(inputMod);
-            const power =
-                pyodide.runPython(`pow(${base}, ${exponent}, ${mod})`);
-            //console.log(`base=${base}, exponent=${exponent}, mod=${mod}`)
+            const exponent = parse(inputExponent, value => value >= 0n);
+            const mod = parse(inputMod, value => value !== 0n);
 
-            resultBase.innerText = String(base);
-            resultExponent.innerText = String(exponent);
-            resultMod.innerText = String(mod);
-            result.innerText = String(power);
-        } catch (SyntaxError) {
-            resultBase.innerText = NO_VALUE;
-            resultExponent.innerText = NO_VALUE;
-            resultMod.innerText = NO_VALUE;
-            result.innerText = NO_VALUE;
+            const expression = `pow(${base}, ${exponent}, ${mod})`;
+            const power = pyodide.runPython(expression);
+
+            setResults(base, exponent, mod, power);
+        } catch (error) {
+            if (error instanceof SyntaxError || error instanceof RangeError) {
+                clearResults();
+            } else {
+                throw error;
+            }
         }
     };
 
