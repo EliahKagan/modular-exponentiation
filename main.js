aa85eb4e5178680ecb6439cc3678400f426ac99b
Eliah Kagan <degeneracypressure@gmail.com>
2021-09-03 13:02:53 -0400

    show any well-formed paramters in output
    This makes it clearer to users what inputs still need to be
    changed. (The only way was also very weird an unintuitive.)

--- main.js
+++ main.js
@@ -1,69 +1,97 @@
 (async function () {
-    const NO_VALUE = '???';
+    const NO_VALUE_TEXT = '???';
 
     const pyodide = await loadPyodide({
         indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.18.0/full/'
     });
-    window.pyodide = pyodide;
 
-    pyodide.runPython(await (await fetch('hello.py')).text());
+    const params = Object.freeze((function () {
+        const ret = {};
 
-    const inputBase = document.getElementById('base');
-    const inputExponent = document.getElementById('exponent');
-    const inputMod = document.getElementById('mod');
-    const resultBase = document.getElementById('result-base');
-    const resultExponent = document.getElementById('result-exponent');
-    const resultMod = document.getElementById('result-mod');
-    const resultPower = document.getElementById('result-power');
+        ['base', 'exponent', 'mod'].forEach(param => {
+            ret[param] = {
+                input: document.getElementById(`input-${param}`),
+                output: document.getElementById(`output-${param}`),
+                value: undefined,
+            };
+        });
 
-    const parse = function (input, rangeCheck = value => true) {
-        const text = input.value.trim();
+        ret.exponent.rangeCheck = value => value >= 0n;
+        ret.mod.rangeCheck = value => value !== 0n;
+
+        return ret;
+    })());
+
+    const outputPower = document.getElementById('output-power');
+
+    const tryParseBigInt = function (text) {
+        text = text.trim();
+
         if (text.length === 0) {
-            throw new SyntaxError('refusing to parse empty string as zero');
+            return undefined; // Refuse to parse an empty string as zero.
         }
 
-        const value = BigInt(text);
-        if (!rangeCheck(value)) {
-            throw new RangeError('an argument is out of range');
-        }
-
-        return value;
-    };
-
-    const clearResults = function () {
-        resultBase.innerText = NO_VALUE;
-        resultExponent.innerText = NO_VALUE;
-        resultMod.innerText = NO_VALUE;
-        resultPower.innerText = NO_VALUE;
-    };
-
-    const setResults = function (base, exponent, mod, power) {
-        resultBase.innerText = String(base);
-        resultExponent.innerText = String(exponent);
-        resultMod.innerText = String(mod);
-        resultPower.innerText = String(power);
-    };
-
-    const update = function () {
         try {
-            const base = parse(inputBase);
-            const exponent = parse(inputExponent, value => value >= 0n);
-            const mod = parse(inputMod, value => value !== 0n);
-
-            const expression = `pow(${base}, ${exponent}, ${mod})`;
-            const power = pyodide.runPython(expression);
-
-            setResults(base, exponent, mod, power);
+            return BigInt(text);
         } catch (error) {
-            if (error instanceof SyntaxError || error instanceof RangeError) {
-                clearResults();
+            if (error instanceof SyntaxError) {
+                return undefined; // The input did not look like an integer.
             } else {
                 throw error;
             }
         }
+    }
+
+    const tryValidateBigInt = function (text, rangeCheck) {
+        const value = tryParseBigInt(text);
+
+        if (value === undefined) {
+            return undefined;
+        }
+        if (rangeCheck === undefined || rangeCheck(value)) {
+            return value;
+        }
+        return undefined;
+    }
+
+    const parse = function (paramData) {
+        value = tryValidateBigInt(paramData.input.value,
+                                  paramData.rangeCheck);
+
+        paramData.value = value;
+
+        if (value === undefined) {
+            paramData.output.innerText = NO_VALUE_TEXT;
+            return false;
+        }
+
+        paramData.output.innerText = String(value);
+        return true;
     };
 
-    inputBase.addEventListener('input', update);
-    inputExponent.addEventListener('input', update);
-    inputMod.addEventListener('input', update);
+    const computePower = function (base, exponent, mod) {
+        return pyodide.runPython(`pow(${base}, ${exponent}, ${mod})`);
+    };
+
+    const update = function () {
+        let ok = true;
+
+        Object.values(params).forEach(paramData => {
+            // Don't short-circuit: update all, even if some are ill-formed.
+            ok = parse(paramData) && ok;
+        });
+
+        if (ok) {
+            const power = computePower(params.base.value,
+                                       params.exponent.value,
+                                       params.mod.value);
+
+            outputPower.innerText = String(power);
+        } else {
+            outputPower.innerText = NO_VALUE_TEXT;
+        }
+    };
+
+    Object.values(params).forEach(paramData =>
+        paramData.input.addEventListener('input', update));
 })();
